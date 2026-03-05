d7973abd2b3920c2d58d4ee2272e988d0513466e
singhanandmdp <anandsinghmdp8@gmail.com>
2026-03-06 00:45:24 +0530

    Add files via upload

--- js/product.js
+++ js/product.js
@@ -1,22 +1,27 @@
-const previewBox = document.querySelector(".preview-box");
-const mainImage = document.getElementById("mainImage");
+const params = new URLSearchParams(window.location.search);
+const name = params.get("name");
 
-previewBox.addEventListener("mousemove", (e) => {
-    const rect = previewBox.getBoundingClientRect();
+fetch("designs.json")
+.then(res => res.json())
+.then(data => {
 
-    const x = ((e.clientX - rect.left) / rect.width) * 100;
-    const y = ((e.clientY - rect.top) / rect.height) * 100;
+    const design = data.find(d => d.name === name);
 
-    mainImage.style.transformOrigin = `${x}% ${y}%`;
-    mainImage.style.transform = "scale(2)";
+    if(!design) return;
+
+    document.querySelector(".product-info h1").innerText = design.name;
+
+    document.getElementById("mainImage").src = design.preview1;
+
+    const thumbs = document.querySelectorAll(".thumbnail-row img");
+
+    thumbs[0].src = design.preview1;
+    thumbs[1].src = design.preview2;
+    thumbs[2].src = design.preview3;
+
+    const btn = document.querySelector(".download-btn");
+    btn.onclick = () => {
+        window.open(design.download, "_blank");
+    };
+
 });
-
-previewBox.addEventListener("mouseleave", () => {
-    mainImage.style.transform = "scale(1)";
-});
-
-/* Thumbnail click */
-function changeImage(el) {
-    mainImage.src = el.src;
-    mainImage.style.transform = "scale(1)";
-}
